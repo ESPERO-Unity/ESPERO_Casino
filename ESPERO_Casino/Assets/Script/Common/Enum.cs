a766f07430287f93a140604ae6b2781e02de3323
matsumotok <k.matsumoto@smatec.jp>
2017-02-27 08:50:48 +0900

    機能追加：画面遷移

---
+++ ESPERO_Casino/Assets/Script/Common/Enum.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Enum : MonoBehaviour {
+
+	public enum EnmScene{
+		Title,
+		Select,
+		BlackJack,
+		Result,
+		Setting
+	}
+}
--- ESPERO_Casino/Assets/Script/Common/SceneEvent.cs
+++ ESPERO_Casino/Assets/Script/Common/SceneEvent.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
 
-public class SceneEvent : MonoBehaviour {
+public class SceneEvent : MonoBehaviour  {
+	
+	/**
+     **  機能：シーンの遷移（引数はUIから入力）
+     **  備考１："public void"でないとUIからOnClick関数を呼び出せない
+     **  備考２：LoadScene関数のインデックスは、"BuildSetting"の"Scene In Build"の設定と連動している
+	 **/
+	public void OnClick(int SceneIdx){
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		SceneManager.LoadScene(SceneIdx);  //引数はSceneのインデックス番号
+
 	}
 }

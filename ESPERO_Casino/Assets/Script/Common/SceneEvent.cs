447f49ecf263918b9ba175a37e04881b53b55d5b
MasashiTakano <m.takano@smatec.jp>
2017-03-02 02:11:16 +0900

    設定画面から遷移元画面へ戻る機能追加

--- ESPERO_Casino/Assets/Script/Common/SceneEvent.cs
+++ ESPERO_Casino/Assets/Script/Common/SceneEvent.cs
@@ -4,13 +4,21 @@ using UnityEngine.SceneManagement;
 
 public class SceneEvent : MonoBehaviour  {
 
+	public static string ReturnScene = "1_Select"; //設定画面からの遷移用
+
+
 	/**
      **  機能：シーンの遷移：Aパターン（引数はUIから入力）
      **  備考１："public void"でないとUIからOnClick関数を呼び出せない
      **  備考２：LoadScene関数のインデックスは、"BuildSetting"の"Scene In Build"の設定と連動している
+     **  備考３：設定画面から遷移する際に呼び出し元を知る必要があるのでシーン名をReturnSceneに保持する
 	 **/
 	public void OnClick_A(int SceneIdx){
 		SceneManager.LoadScene(SceneIdx);  //引数はSceneのインデックス番号
+
+		Scene scene = SceneManager.GetActiveScene();
+		ReturnScene = scene.name;
+
 	}
 
 
@@ -27,5 +35,16 @@ public class SceneEvent : MonoBehaviour  {
 		yield return new WaitForSeconds(2f);	// 2秒間待つ
 		SceneManager.LoadScene(SceneIdx);
 	}
+
+	/**
+     **  機能：シーンの遷移：Cパターン（引数はなし）
+     **  備考１：シーンの移動先が固定ではなく、呼び出し元にいく
+     **  備考２：OnClick_Aと合わせる感じでint型が良かったが、インデックを取得するメソッドが分からなかった
+	 **/
+	public void OnClick_C(){
+		SceneManager.LoadScene(ReturnScene);  //引数はScene名
+
+	}
+
 
 }

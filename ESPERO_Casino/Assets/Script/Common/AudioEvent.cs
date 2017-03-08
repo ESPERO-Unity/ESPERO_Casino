488ba560ce7cc6a3566a8703cea62dd60b2f91a4
matsumotok <k.matsumoto@smatec.jp>
2017-03-08 20:14:08 +0900

    Audio操作スクリプトのテンプレート作成

---
+++ ESPERO_Casino/Assets/Script/Common/AudioEvent.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/*動作確認は未実施*/
+
+
+public class AudioEvent : MonoBehaviour {
+
+    private AudioSource audioSource;   //AudioSourceコンポーネント
+    
+	/**
+     **  機能：AudioSourceコンポーネントのセット
+     **  備考１："gameObject"は、そのスクリプトが属しているオブジェクトを指す。
+     **           データ型の"GameObject"とは異なるので注意。
+     **  備考２：AudioSourceコンポーネントの派生メソッドはこのスクリプト内で定義するので、
+     **          この関数は"private"としている。
+	 **/
+	private void SetAudio(){
+	    audioSource = gameObject.GetComponent<AudioSource>();
+	}
+	
+	/**
+     **  機能：音量コントローラ
+     **  備考：音量を調整する場合は、設定値を変更する。
+	 **/
+	public void Volume(){
+		SetAudio();
+		audioSource.volume = 1.0f;
+	}
+
+	/**
+     **  機能：オーディオ再生
+	 **/
+	public void Play(){
+		SetAudio();
+		audioSource.Play();
+	}
+
+	/**
+     **  機能：オーディオ一時停止
+	 **/
+	public void Pause(){
+		SetAudio();
+		audioSource.Pause();
+	}
+
+	/**
+     **  機能：オーディオ完全停止
+	 **/
+	public void Stop(){
+		SetAudio();
+		audioSource.Stop();
+	}
+
+}
---
+++ ESPERO_Casino/Assets/Script/Common/ButtonEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonEvent : MonoBehaviour {
+
+    private Button button;   //Buttonコンポーネント
+    
+	/**
+     **  機能：Buttonコンポーネントのセット
+	 **/
+	private void SetButton(){
+		button = gameObject.GetComponent<Button>();
+	}
+	
+	}
